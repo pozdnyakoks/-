6034cdead22ebe94b58afdffa0b63ab76fccfae7
pozdnyakoks <pozdnyakoks@mail.ru>
2024-02-02 09:32:53 -0300

    fix error

--- utils/scrollbar.tsx
+++ utils/scrollbar.tsx
@@ -7,26 +7,28 @@ export const useScrollbarWidth = () => {
   const widthRef = React.useRef(0);
 
   if (didCompute.current) return widthRef.current;
+  if (typeof document !== 'undefined') {
 
-  // Creating invisible container
-  const outer = document.createElement('div');
-  outer.style.visibility = 'hidden';
-  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-  // outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-  document.body.appendChild(outer);
+    // Creating invisible container
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+    // outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+    document.body.appendChild(outer);
 
-  // Creating inner element and placing it in the container
-  const inner = document.createElement('div');
-  outer.appendChild(inner);
+    // Creating inner element and placing it in the container
+    const inner = document.createElement('div');
+    outer.appendChild(inner);
 
-  // Calculating difference between container's full width and the child width
-  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
 
-  // Removing temporary elements from the DOM
-  if (outer.parentNode !== null)
-  outer.parentNode.removeChild(outer);
+    // Removing temporary elements from the DOM
+    if (outer.parentNode !== null)
+      outer.parentNode.removeChild(outer);
 
-  didCompute.current = true;
-  widthRef.current = scrollbarWidth;
-  return scrollbarWidth;
+    didCompute.current = true;
+    widthRef.current = scrollbarWidth;
+    return scrollbarWidth;
+  }
 };
