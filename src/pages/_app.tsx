0b7c38acc29f9224bf00a1e6b9712b19f0bfaa5a
pozdnyakoks <pozdnyakoks@mail.ru>
2024-02-06 19:53:51 -0300

    fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,22 @@ import type { AppProps } from 'next/app';
 import '@/styles/globals.scss';
 import RootLayout from '@/components/layout';
 import { Urbanist } from "next/font/google";
+import { Provider } from 'react-redux';
+import store from '@/lib/store';
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
-    <div className={urbanist.className}>
-      <RootLayout >
-        <Component {...pageProps} />
-      </RootLayout>
-    </div>
+    <Provider store={store}>
+
+      <div className={urbanist.className}>
+        <RootLayout >
+          <Component {...pageProps} />
+        </RootLayout>
+      </div>
+    </Provider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,66 @@
 'use client'
 
-import store from "@/lib/store";
-import { Provider } from "react-redux";
+import store, { RootState } from "@/lib/store";
+import { Provider, useSelector } from "react-redux";
 import { Hero } from "@/components/hero/Hero";
 import { Vacancies } from "@/components/vacancies/Vacancies";
 import { getVacancies } from "@/api/vacancies";
+import Airtable, { FieldSet } from "airtable";
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
-  // getVacancies();
+interface Props {
+  fetchedJobs: FieldSet[];
+}
+
+export default function Home({ fetchedJobs }: { fetchedJobs: FieldSet[] }) {
+  const currentJob = useSelector(
+    (state: RootState) => state.job.job
+  );
+ 
+  console.log(currentJob)
+  console.log(fetchedJobs)
 
   return (
-    <Provider store={store}>
-        <Hero />
-        <Vacancies />
-    </Provider>
+    <>
+      <Hero />
+      <Vacancies />
+    </>
+
   );
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+ Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.API_TOKEN
+  });
+
+  const base = Airtable.base('appuWSjhfaZZZfaRo');
+  const records: FieldSet[] = await new Promise((resolve, reject) => {
+    const fetchedJobs: FieldSet[] = [];
+    base('Jobs').select({
+      // maxRecords: 15,
+      view: "Grid view"
+    }).eachPage(function page(pageRecords, fetchNextPage) {
+      pageRecords.forEach(record => {
+        fetchedJobs.push(record.fields);
+      });
+      fetchNextPage();
+    }, function done(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(fetchedJobs);
+      }
+    });
+  });
+
+  return {
+    props: {
+      fetchedJobs: records,
+    },
+  };
+};
 
 
 
