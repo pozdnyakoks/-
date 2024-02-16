3ccffaa61c01bf4586ac740bc32d5960145f4520
pozdnyakoks <pozdnyakoks@mail.ru>
2024-02-16 12:11:55 -0300

    fix og

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -5,29 +5,12 @@ export const config = {
   runtime: 'edge',
 };
 
-export default async function handler() {
-  const imageData = await fetch(new URL('/OG.png', import.meta.url)).then(
-    (res) => res.arrayBuffer(),
-  );
-
-  const base64ImageData = Buffer.from(imageData).toString('base64');
-  const src = `data:image/png;base64,${base64ImageData}`;
+export default function handler() {
+  const src = '/OG.png'; // Путь к изображению в папке public
 
   return new ImageResponse(
     (
-      <div
-        style={{
-          display: 'flex',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Image width={256} height={256} src={src} alt='Jobs in Cosmos Blockchain' />
-      </div>
+        <Image width={1200} height={630} src={src} alt='Jobs in Cosmos Blockchain' />
     ),
     {
       width: 1200,
