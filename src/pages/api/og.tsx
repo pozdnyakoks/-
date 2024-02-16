9e60c16f97160f3165f40970a13cd775d8197bae
pozdnyakoks <pozdnyakoks@mail.ru>
2024-02-16 12:15:57 -0300

    fix og

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,20 +1,34 @@
-import { ImageResponse } from '@vercel/og';
-import Image from 'next/image';
+// import { ImageResponse } from '@vercel/og';
+// import Image from 'next/image';
 
-export const config = {
-  runtime: 'edge',
+// export const config = {
+//   runtime: 'edge',
+// };
+
+// export default function handler() {
+//   const src = '/OG.png'; // Путь к изображению в папке public
+
+//   return new ImageResponse(
+//     (
+//         <Image width={1200} height={630} src={src} alt='Jobs in Cosmos Blockchain' />
+//     ),
+//     {
+//       width: 1200,
+//       height: 630,
+//     },
+//   );
+// }
+import { NextApiHandler } from 'next';
+import fs from 'fs';
+import path from 'path';
+
+const handler: NextApiHandler = (req, res) => {
+  const src = '/OG.png'; // Путь к изображению в папке public
+  const filePath = path.join(process.cwd(), 'public', src);
+  const fileContents = fs.readFileSync(filePath);
+
+  res.setHeader('Content-Type', 'image/png');
+  res.status(200).send(fileContents);
 };
 
-export default function handler() {
-  const src = '/OG.png'; // Путь к изображению в папке public
-
-  return new ImageResponse(
-    (
-        <Image width={1200} height={630} src={src} alt='Jobs in Cosmos Blockchain' />
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
-}
+export default handler;
