ec5583b6e451f627794d4b19ca4e0049db0f46cc
pozdnyakoks <pozdnyakoks@mail.ru>
2024-02-23 10:33:23 -0300

    feat: add metadata to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,28 @@
 import Head from "next/head";
 import { About } from "@/components/about/About";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Incosmos.Work",
+  description: "The only job board in Cosmos Blockchain",
+  openGraph: { images: ['/og.png'] },
+  icons: {
+    icon: '/favicon.ico',
+  }
+};
 
 export default function AboutPage() {
 
   return (
     <>
-      <Head>
+      {/* <Head>
       <meta name="title"
           content="About Incosmos.Work" />
         <meta name="description"
           content="The only job board in Cosmos Blockchain" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>About Incosmos.Work</title>
-      </Head>
+      </Head> */}
       <About />
     </>
   )
